3d829711ebe953bee7733b87c53bc10c65bfce95
kaviyavarma08 <kaviyadhiv567@gmail.com>
2025-03-14 22:06:04 +0530

    Time: 5 ms (70.66%), Space: 44.3 MB (83.84%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        ListNode curr = head;
+        while(curr != null){
+            pq.add(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        while(curr != null){
+            curr.val = pq.poll();
+            curr = curr.next;
+        }
+        return head;
+    }
+}
